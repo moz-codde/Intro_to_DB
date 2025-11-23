b033d209b6fe7b4622eae2c8e6db788175f39670
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-23 17:35:50 +0100

    MySQLServer.py: Initial commit

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="moz.codde",
+        database="alx_book_store",
+    )
+except:
+    print("Error: Failed to connect to the DB")
+
+my_cursor = mydb.cursor()
+
+my_cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+print("Database 'alx_book_store' created successfully!")
+
+
+
+my_cursor.close()
+mydb.close()
